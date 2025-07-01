b70680fd643ddf2b77394d8ecad8949f80a5cf9f
adachng <11900409+adachng@users.noreply.github.com>
2025-07-01 22:30:26 +0800

    Make `enum GridCellState` public for graphics

--- src/snake_game_manager/enum_priv.h
+++ src/snake_game_manager/enum_priv.h
@@ -2,14 +2,6 @@
 #ifndef SRC_SNAKE_GAME_MANAGER_ENUM_PRIV_H
 #define SRC_SNAKE_GAME_MANAGER_ENUM_PRIV_H
 
-enum GridCellState
-{
-    CELL_EMPTY = 0U,
-    CELL_APPLE = 1U << 0U,
-    CELL_SNAKE_HEAD = 1U << 1U,
-    CELL_SNAKE_BODY = 1U << 2U,
-}; // enum SceneGridPointState
-
 enum SnakeDirection
 {
     DIRECTION_UP = 'w',
--- src/snake_game_manager/snake_game_manager.h
+++ src/snake_game_manager/snake_game_manager.h
@@ -4,6 +4,14 @@
 
 #include <stdbool.h>
 
+enum GridCellState
+{
+    CELL_EMPTY = 0U,
+    CELL_APPLE = 1U << 0U,
+    CELL_SNAKE_HEAD = 1U << 1U,
+    CELL_SNAKE_BODY = 1U << 2U,
+}; // enum SceneGridPointState
+
 struct SnakeGameManager;
 /**
  * @brief Constructor of SnakeGameManager (dynamically allocates).
